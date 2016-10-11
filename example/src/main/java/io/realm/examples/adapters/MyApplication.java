f4232748c91944fc18775a40cefd9ed4e15e7be6
Lars Grefer <github@larsgrefer.de>
2016-10-11 16:19:18 +0200

    Update build dependencies (#62)
    * update gradle to 3.1
    
    * update Realm to 2.0.0
    
    * update Android API to 24
    
    * remove deprecated plugin
    
    * update Realm to 2.0.1
    
    * update buildTools to 24.0.3
    
    * fix tests
    
    * update realm to 2.0.2

--- example/src/main/java/io/realm/examples/adapters/MyApplication.java
+++ example/src/main/java/io/realm/examples/adapters/MyApplication.java
@@ -26,7 +26,8 @@ public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        RealmConfiguration realmConfig = new RealmConfiguration.Builder(this).build();
+        Realm.init(this);
+        RealmConfiguration realmConfig = new RealmConfiguration.Builder().build();
         Realm.deleteRealm(realmConfig); // Delete Realm between app restarts.
         Realm.setDefaultConfiguration(realmConfig);
     }
